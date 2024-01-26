ef30c811b7956a42423d69f55cadbf025018e1a5
Ilya_century <61396442+Sabevvam@users.noreply.github.com>
2024-01-26 05:57:37 +0200

    Add files via upload

---
+++ LeetCode/2235. Add Two Integers/2235. Add Two Integers.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        result = num1 + num2
+        return result
+
+solution = Solution()
+num1 = 12
+num2 = 15
+result_test = solution.sum(num1, num2)
+print(result_test)
